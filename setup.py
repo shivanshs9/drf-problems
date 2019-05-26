98491914d1bafd6f1aab08512e5f1095112a37c1
Shivansh Saini <shivanshs9@gmail.com>
2019-05-26 13:28:21 +0530

    Added License

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
@@ -8,13 +8,13 @@ README = open(os.path.join(here, 'README.md')).read()
 setup(
     name='drf-problems',
     version='0.1',
-    packages=['drf_problems'],
     description='Never liked DRF plain old errors? Forget that and accept RFC 7807 - Problem Details!',
     long_description=README,
     long_description_content_type="text/markdown",
     author='shivanshs9',
     author_email='shivanshs9@gmail.com',
     url='https://github.com/shivanshs9/drf-problems/',
+    packages=find_packages(),
     license='MIT',
     install_requires=[
         'Django>=1.6',
