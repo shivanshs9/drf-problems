aac042e3e8fda78df3c2d6fafcdbddf4a1970fbf
Shivansh Saini <shivanshs9@gmail.com>
2019-05-26 15:05:21 +0530

    Added proper versioning in setup

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 import os
+import re
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
 
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
 setup(
     name='drf-problems',
-    version='0.1',
+    version=get_version('drf_problems'),
     description='Never liked DRF plain old errors? Forget that and accept RFC 7807 - Problem Details!',
     long_description=README,
     long_description_content_type="text/markdown",
