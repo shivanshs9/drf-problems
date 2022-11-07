4784b9edd78ca67664ec2e163c7f1c5e1a0ed5cc
pietro convalle <pietroconvalle@python.it>
2022-11-07 15:56:57 +0000

    formatting with single quotes

--- drf_problems/exceptions.py
+++ drf_problems/exceptions.py
@@ -10,7 +10,7 @@ from rest_framework.views import exception_handler as drf_exception_handler
 
 from drf_problems.utils import register
 
-logger = logging.getLogger("drf_problems")
+logger = logging.getLogger('drf_problems')
 
 
 def exception_handler(exc, context):
@@ -24,26 +24,26 @@ def exception_handler(exc, context):
         logger.exception(exc)
         exc = exceptions.APIException(exc)
 
-    request = context["request"]
+    request = context['request']
     response = drf_exception_handler(exc, context)
     data = response.data
 
-    problem_title = getattr(exc, "title", exc.default_detail)
+    problem_title = getattr(exc, 'title', exc.default_detail)
     problem_status = response.status_code
-    problem_code = getattr(exc, "code", exc.default_code)
+    problem_code = getattr(exc, 'code', exc.default_code)
     problem_type = reverse(
-        "drf_problems:error-documentation",
-        kwargs={"code": problem_code},
+        'drf_problems:error-documentation',
+        kwargs={'code': problem_code},
         request=request,
     )
-    problem_instance = getattr(exc, "instance", None)
-    problem_extensions = getattr(exc, "extensions", {})
+    problem_instance = getattr(exc, 'instance', None)
+    problem_extensions = getattr(exc, 'extensions', {})
     if isinstance(data, dict):
-        data["title"] = problem_title
-        data["status"] = problem_status
-        data["type"] = problem_type
+        data['title'] = problem_title
+        data['status'] = problem_status
+        data['type'] = problem_type
         if problem_instance:
-            data["instance"] = problem_instance
+            data['instance'] = problem_instance
         data.update(problem_extensions)
     else:
         data = dict(
@@ -53,8 +53,8 @@ def exception_handler(exc, context):
             type=problem_type,
         )
     try:
-        if request.accepted_renderer.format == "json":
-            response.content_type = "application/problem+json"
+        if request.accepted_renderer.format == 'json':
+            response.content_type = 'application/problem+json'
     except AttributeError:
         pass
     response.data = data
@@ -64,11 +64,11 @@ def exception_handler(exc, context):
 
 @register
 class InvalidVersionRequestedException(exceptions.NotAcceptable):
-    default_code = "invalid_version"
-    default_detail = _("Invalid API version provided.")
+    default_code = 'invalid_version'
+    default_detail = _('Invalid API version provided.')
     format_detail = _('Provided version "{request_version}" is invalid.')
     description = _(
-        "Malformed or unsupported version string is provided with the request."
+        'Malformed or unsupported version string is provided with the request.'
     )
 
     def __init__(self, request_version, detail=None, code=None):
@@ -81,13 +81,13 @@ class InvalidVersionRequestedException(exceptions.NotAcceptable):
 
 @register
 class DeprecatedVersionUsedException(exceptions.PermissionDenied):
-    default_code = "deprecated_version"
-    default_detail = _("Deprecated API version provided.")
+    default_code = 'deprecated_version'
+    default_detail = _('Deprecated API version provided.')
     format_detail = _(
         'Minimum version supported is "{min_version}", but the request used "{request_version}"'
     )
     description = _(
-        "API only supports versions above the minimum requirement."
+        'API only supports versions above the minimum requirement.'
     )
 
     def __init__(self, request_version, min_version, detail=None, code=None):
